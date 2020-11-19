a868d700e404a6b08253e3c8fa6ec7862860393a
Missy Maloney <mmaloney@healthstarInfo.com>
2020-11-18 22:10:06 -0500

    initial commit

---
+++ server/index.js
@@ -0,0 +1,32 @@
+// Main starting point of the app
+
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+
+// DB Setup
+mongoose.connect('mongodb://localhost/auth', { useNewUrlParser: true,
+    useUnifiedTopology: true});
+
+const connection = mongoose.connection;
+
+connection.on("connected", function() {
+    console.log('connected to db');
+});
+
+
+//  App Setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*'}));
+router(app);
+
+
+//  Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on:', port);
