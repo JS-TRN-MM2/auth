472bd1b886a6c3dbb77a6242ed43b569a31f48e5
Missy Maloney <mmaloney@healthstarInfo.com>
2020-11-21 12:41:40 -0500

    added client code

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB Setup
 mongoose.connect('mongodb://localhost/auth', { useNewUrlParser: true,
@@ -21,6 +22,7 @@ connection.on("connected", function() {
 
 //  App Setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*'}));
 router(app);
 
